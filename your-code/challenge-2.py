b676cd708ef20a54d0d9d5bec145c082458c8def
Rodolfo Pardo <rodypardo@gmail.com>
2019-07-17 17:43:42 -0500

    Subiendo tarea

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -1,38 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-The code below generates a given number of random strings that consists of numbers and 
-lower case English letters. You can also define the range of the variable lengths of
-the strings being generated.
+Created on Wed Jul 17 16:08:26 2019
 
-The code is functional but has a lot of room for improvement. Use what you have learned
-about simple and efficient code, refactor the code.
+@author: rodolfopardo
 """
+#GEnerador de alfanumerico random
+#La idea fue sintetizar todo en una funcion que solo ejecute de acuerdo al largo de palabras que seleccione usuario
+#Se realiza en una funcion 
+#Se importan las librerias random y string
+#While con un try y except para evitar errores
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
+import string
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+def random_string(*args):
+    letters = string.ascii_lowercase + string.digits
+    str_final = ''.join(random.choice(letters) for i in range(size))
+    return str_final
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+count = 5
+while count > 0:
+       try:    
+           size = int(input("De cuantos caracteres quiere generar un string aleatorio: "))
+           print(random_string(size))
+           x = input("¿Desea salir?: (responda si o no)")
+           if x == "si" or x == 'yes':
+                print("Ok, que tengas un buen dia")
+                break;
+           else:
+             print("Sigamos generando strings!")
+       except:
+            print("Por favor, ingrese un numero, no letra. Tiene %s chances" % count)
+            count = count - 1
+
+
