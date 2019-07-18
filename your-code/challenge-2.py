73a8e2c551cb38afb6b97b855f90c6cf54d9989a
Emilio Villalobos <evillalobos1123@gmail.com>
2019-07-17 22:41:35 -0500

    DE Emilio code simplicity

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -1,38 +1,21 @@
-"""
-The code below generates a given number of random strings that consists of numbers and 
-lower case English letters. You can also define the range of the variable lengths of
-the strings being generated.
+def many(minlen, maxlen, many):
+    todas = []
+    for x in range(many):
+        y = randomString(minlen, maxlen)
+        todas.append(y)
+    return todas
 
-The code is functional but has a lot of room for improvement. Use what you have learned
-about simple and efficient code, refactor the code.
-"""
-
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
-
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
-
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
+def randomString(minlen, maxlen):
+    import random
+    import string
+    letterr = list(string.ascii_lowercase)
+    for x in range(0,10):
+        letterr.append(x) 
+    rand = [str(random.choice(letterr)) for i in range(maxlen+1)]        
+    return "".join(rand)
+    
+a = input('Enter minimum string length (between 1 and 12): ')
+b = input('Enter maximum string length (between 1 and 12): ')
 n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(many(int(a), int(b), int(n)))
